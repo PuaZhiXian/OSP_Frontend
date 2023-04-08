2006de348fad00e9c59bd52ad5a21fce7e4ac131
PuaZhiXian <xianpua2001@gmail.com>
2023-04-08 12:56:57 +0800

    create article detail page

--- src/app/pages/article/article.module.ts
+++ src/app/pages/article/article.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {ArticleDetailComponent} from "./page/article-detail/article-detail.component";
 import {ArticleRoutingModule} from "./article-routing.module";
+import {NzImageModule} from "ng-zorro-antd/image";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
-  imports: [ArticleRoutingModule],
+  imports: [ArticleRoutingModule, NzImageModule, CommonModule],
   declarations: [
     ArticleDetailComponent
   ],
